3b81d4a957181e81a30075f3e0eb64e89b29cf15
GitHub Action <action@github.com>
2025-02-26 07:21:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/p5656zx/claude2plugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/p5656zx/claude2plugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.p5656zx.claude2plugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
